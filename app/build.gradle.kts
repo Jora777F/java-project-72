d5187ff6a6db00daa372ab4e52cd354737fb552d
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-11-20 14:53:18 +0300

    feature : add a javalin and slf4j to the project
    * Added a Javalin and Slf4j to the project

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,6 +18,9 @@ application {
 }
 
 dependencies {
+    implementation("io.javalin:javalin:6.7.0")
+    implementation("org.slf4j:slf4j-simple:2.0.9")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
