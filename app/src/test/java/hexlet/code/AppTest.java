685ea10984aae155a2cd27ea32f38601f6caad94
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-11-20 13:02:05 +0300

    feature : add a jacoco plugin to the project
    * Added a JaCoCo plugin to the project

---
+++ app/src/test/java/hexlet/code/AppTest.java
@@ -0,0 +1,13 @@
+package hexlet.code;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class AppTest {
+
+    @Test
+    void simpleTest() {
+        assertEquals(2, 1 + 1);
+    }
+}
