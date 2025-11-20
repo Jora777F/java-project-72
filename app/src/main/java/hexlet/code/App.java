33142433c741e93231eac3fa50b1bcc98aeb2397
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-11-20 15:28:58 +0300

    refactor : edit the app
    * Edited the App class

--- app/src/main/java/hexlet/code/App.java
+++ app/src/main/java/hexlet/code/App.java
@@ -1,18 +1,27 @@
 package hexlet.code;
 
 import io.javalin.Javalin;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class App {
 
-    public static final int PORT = 7070;
+    private static final Logger LOGGER = LoggerFactory.getLogger(App.class);
+    public static final String DEFAULT_PORT = "7070";
 
     public static void main(String[] args) {
         Javalin app = getApp();
-        app.start(PORT);
+        app.start(getPort());
     }
 
     public static Javalin getApp() {
         return Javalin.create()
                 .get("/", ctx -> ctx.result("Hello World"));
+    }
+
+    private static int getPort() {
+        String port = System.getenv().getOrDefault("PORT", DEFAULT_PORT);
+        LOGGER.debug("Using port: {}", port);
+        return Integer.parseInt(port);
     }
 }
