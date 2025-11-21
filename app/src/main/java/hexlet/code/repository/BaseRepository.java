93cda11c74ecb8b95d41dda6f24f9f566ea7963c
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-11-21 14:58:54 +0300

    feature : add a base repository
    * Added a base repository

---
+++ app/src/main/java/hexlet/code/repository/BaseRepository.java
@@ -0,0 +1,12 @@
+package hexlet.code.repository;
+
+import javax.sql.DataSource;
+
+public class BaseRepository {
+
+    protected DataSource dataSource;
+
+    public BaseRepository(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+}
