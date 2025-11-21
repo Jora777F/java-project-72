81d00ea0e7458766e6992493c0a170ead558dd2f
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-11-21 13:54:25 +0300

    feature : add a hikari connection pool
    * Added the HikariCP
    * Added a PostgreSQL driver

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -21,6 +21,9 @@ dependencies {
     implementation("io.javalin:javalin:6.7.0")
     implementation("org.slf4j:slf4j-simple:2.0.9")
 
+    implementation("com.zaxxer:HikariCP:7.0.2")
+    implementation("org.postgresql:postgresql:42.7.3")
+
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
