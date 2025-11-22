d608ca9458ea0663aa35ab646ae42754b2fed288
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-11-22 12:52:15 +0300

    feature : add a named routes
    * Added the NamedRoutes to the project

---
+++ app/src/main/java/hexlet/code/util/NamedRoutes.java
@@ -0,0 +1,35 @@
+package hexlet.code.util;
+
+public final class NamedRoutes {
+    private static final String ROOT_PATH = "/";
+    private static final String URLS = "/urls";
+    private static final String URLS_BASE_PATH = "/urls/";
+
+    private NamedRoutes() {
+        throw new UnsupportedOperationException("Это служебный класс, создание экземпляров запрещено");
+    }
+
+    public static String rootPath() {
+        return ROOT_PATH;
+    }
+
+    public static String urlsPath() {
+        return URLS;
+    }
+
+    public static String urlPath(Long id) {
+        return URLS_BASE_PATH + id;
+    }
+
+    public static String urlPath(String id) {
+        return URLS_BASE_PATH + id;
+    }
+
+    public static String urlChecksPath(String urlId) {
+        return URLS_BASE_PATH + urlId + "/checks";
+    }
+
+    public static String urlChecksPath(Long urlId) {
+        return URLS_BASE_PATH + urlId + "/checks";
+    }
+}
