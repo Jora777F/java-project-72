059cfc99dc43f1a02352c7862724633e5c4ff9d9
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-11-22 15:26:01 +0300

    feature : add the message status
    * Added the message status

---
+++ app/src/main/java/hexlet/code/util/Messages.java
@@ -0,0 +1,8 @@
+package hexlet.code.util;
+
+public class Messages {
+    public static final String SUCCESS = "Страница успешно добавлена.";
+    public static final String FOUND = "Страница уже существует.";
+    public static final String INCORRECT_URL = "Некорректный URL.";
+    public static final String CHECK_SUCCESS = "Страница успешно проверена.";
+}
