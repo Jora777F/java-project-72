04357a352e7bde6376556478383f6cffe8db2742
Mikhail Gorbatenkov <mikh.gorbatenkoff@yandex.ru>
2025-11-24 15:21:12 +0300

    feature : add a url check model
    * Added the UrlCheck model

---
+++ app/src/main/java/hexlet/code/model/UrlCheck.java
@@ -0,0 +1,37 @@
+package hexlet.code.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.sql.Timestamp;
+import java.time.format.DateTimeFormatter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class UrlCheck {
+    private Long id;
+    private int statusCode;
+    private String title;
+    private String h1;
+    private String description;
+    private Long urlId;
+    private Timestamp createdAt;
+
+    public UrlCheck(int statusCode, String title, String h1, String description, Long urlId, Timestamp createdAt) {
+        this.statusCode = statusCode;
+        this.title = title;
+        this.h1 = h1;
+        this.description = description;
+        this.urlId = urlId;
+        this.createdAt = createdAt;
+    }
+
+    public String getFormattedCreatedAt() {
+        return createdAt.toLocalDateTime()
+                .format(DateTimeFormatter.ofPattern("dd.MM.yyyy HH:mm"));
+    }
+}
